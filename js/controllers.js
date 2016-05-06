a842629ac6f48fb2633e36c6507d380028bfc954
Jonathan  Jara Morales <jjara>
2016-05-06 09:12:37 -0600

    JJ: Solution solution 1 (step-1-solution)

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,9 @@
 app.controller('LoginController', function($scope) {
+  $scope.user = {email: '', password: ''};
+
+  $scope.signIn = function() {
+    console.log('Email ' + $scope.user.email);
+    console.log('Password ' + $scope.user.password);
+  }
 
 });
